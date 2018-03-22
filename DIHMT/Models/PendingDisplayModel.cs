19d2925672185f2b287f3bb57423de801ceee9d5
Simon Afek Jensen <simon@axla.dk>
2018-03-22 23:01:08 +0100

    Nullref fixes...

--- DIHMT/Models/PendingDisplayModel.cs
+++ DIHMT/Models/PendingDisplayModel.cs
@@ -18,7 +18,7 @@ namespace DIHMT.Models
         public string RatingExplanation { get; set; }
 
         [DisplayName("Description")]
-        public string ShortExplanation => RatingExplanation.Length > 50 ? $"{RatingExplanation.Substring(0, 46)}..." : RatingExplanation;
+        public string ShortExplanation => RatingExplanation?.Length > 50 ? $"{RatingExplanation.Substring(0, 46)}..." : RatingExplanation;
 
         [DisplayName("Submission timestamp")]
         public DateTime TimeOfSubmission { get; set; }
@@ -26,7 +26,7 @@ namespace DIHMT.Models
         public string SubmitterIp { get; set; }
 
         [DisplayName("Hash of submitter's IP")]
-        public string ShortSubmitterIp => SubmitterIp.Length > 45 ? $"{SubmitterIp.Substring(0, 40)}..." : SubmitterIp;
+        public string ShortSubmitterIp => SubmitterIp?.Length > 45 ? $"{SubmitterIp.Substring(0, 40)}..." : SubmitterIp;
 
         public string SubmitAction { get; set; }
         public List<int> Flags { get; set; }
