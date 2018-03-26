b88a0b63d9f76ca56cf28b0ff2897a1a02e7645c
Simon AJ <simonaj92@gmail.com>
2018-03-26 10:23:15 +0200

    Fix broken else() in Search.cshtml, changed search results to display using Mini.cshtml

--- DIHMT/Scripts/initialize-site.js
+++ DIHMT/Scripts/initialize-site.js
@@ -1,13 +1,3 @@
 ﻿$(function() {
     $('[data-toggle="popover"]').popover();
 });
-
-//document.addEventListener("DOMContentLoaded", function () {
-//    var el = $(".game-img-small");
-
-//    window.onresize = function () {
-//        el.each(function (index, value) {
-//            value.style.width = value.clientHeight + "px";
-//        });
-//    };
-//});
