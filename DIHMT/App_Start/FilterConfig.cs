babf53555e8329dfc24bc7631f0104a9257cc8f6
Simon AJ <simonaj92@gmail.com>
2018-04-09 16:16:57 +0200

    Add dummyimage.com to CSP until Paypal/Patreon icons are ready

--- DIHMT/App_Start/FilterConfig.cs
+++ DIHMT/App_Start/FilterConfig.cs
@@ -14,7 +14,7 @@ namespace DIHMT
             filters.Add(new CspDefaultSrcAttribute { None = true });
             filters.Add(new CspScriptSrcAttribute { Self = true, CustomSources = "platform.twitter.com cdn.syndication.twitter.com cdn.syndication.twimg.com" });
             filters.Add(new CspStyleSrcAttribute { Self = true, UnsafeInline = true, CustomSources = "fonts.googleapis.com platform.twitter.com ton.twimg.com" });
-            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "www.giantbomb.com syndication.twitter.com pbs.twimg.com platform.twitter.com ton.twimg.com data:" });
+            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "www.giantbomb.com syndication.twitter.com pbs.twimg.com platform.twitter.com ton.twimg.com dummyimage.com data:" });
             filters.Add(new CspFontSrcAttribute { CustomSources = "fonts.gstatic.com" });
             filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
             filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
