df5c450baa129ebb8bc4816472cb7fa5b99717e4
Simon AJ <simonaj92@gmail.com>
2018-04-10 12:49:59 +0200

    PayPal button

--- DIHMT/App_Start/FilterConfig.cs
+++ DIHMT/App_Start/FilterConfig.cs
@@ -13,11 +13,11 @@ namespace DIHMT
             filters.Add(new CspDefaultSrcAttribute { None = true });
             filters.Add(new CspScriptSrcAttribute { Self = true, CustomSources = "platform.twitter.com cdn.syndication.twitter.com cdn.syndication.twimg.com" });
             filters.Add(new CspStyleSrcAttribute { Self = true, UnsafeInline = true, CustomSources = "fonts.googleapis.com platform.twitter.com ton.twimg.com" });
-            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "www.giantbomb.com syndication.twitter.com pbs.twimg.com platform.twitter.com ton.twimg.com dummyimage.com data:" });
+            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "www.giantbomb.com www.paypalobjects.com syndication.twitter.com pbs.twimg.com platform.twitter.com ton.twimg.com dummyimage.com data:" });
             filters.Add(new CspFontSrcAttribute { CustomSources = "fonts.gstatic.com" });
             filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
             filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
-            filters.Add(new CspConnectSrcAttribute { Self = true });
+            filters.Add(new CspConnectSrcAttribute { Self = true, CustomSources = "www.paypal.com"});
             filters.Add(new CspReportUriAttribute { ReportUris = "https://mtxz.report-uri.com/r/d/csp/enforce" });
         }
     }
