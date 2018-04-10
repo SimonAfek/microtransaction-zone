8d6fc0098a3049043b260a36ee46cbf0f4d5ed22
Simon AJ <simonaj92@gmail.com>
2018-04-10 15:27:49 +0200

    Disallow embedding MTXZ in other websites via CSP

--- DIHMT/App_Start/FilterConfig.cs
+++ DIHMT/App_Start/FilterConfig.cs
@@ -17,7 +17,8 @@ namespace DIHMT
             filters.Add(new CspFontSrcAttribute { CustomSources = "fonts.gstatic.com" });
             filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
             filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "platform.twitter.com syndication.twitter.com" });
-            filters.Add(new CspConnectSrcAttribute { Self = true, CustomSources = "www.paypal.com"});
+            filters.Add(new CspConnectSrcAttribute { Self = true, CustomSources = "www.paypal.com" });
+            filters.Add(new CspFrameAncestorsAttribute { None = true });
             filters.Add(new CspReportUriAttribute { ReportUris = "https://mtxz.report-uri.com/r/d/csp/enforce" });
         }
     }
