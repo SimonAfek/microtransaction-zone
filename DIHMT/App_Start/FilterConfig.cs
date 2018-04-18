02bc074df7a512c96d53e4906f371dcf2a9d4c22
Simon AJ <simonaj92@gmail.com>
2018-04-18 22:38:51 +0200

    Workaround for Safari being shitty

--- DIHMT/App_Start/FilterConfig.cs
+++ DIHMT/App_Start/FilterConfig.cs
@@ -11,12 +11,12 @@ namespace DIHMT
 
             filters.Add(new CspAttribute());
             filters.Add(new CspDefaultSrcAttribute { None = true });
-            filters.Add(new CspScriptSrcAttribute { Self = true, CustomSources = "www.google.com www.gstatic.com cdnjs.cloudflare.com platform.twitter.com cdn.syndication.twitter.com cdn.syndication.twimg.com" });
-            filters.Add(new CspStyleSrcAttribute { Self = true, UnsafeInline = true, CustomSources = "fonts.googleapis.com platform.twitter.com ton.twimg.com" });
-            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "www.giantbomb.com www.paypalobjects.com syndication.twitter.com pbs.twimg.com platform.twitter.com ton.twimg.com data:" });
-            filters.Add(new CspFontSrcAttribute { CustomSources = "fonts.gstatic.com" });
-            filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "www.google.com platform.twitter.com syndication.twitter.com" });
-            filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "www.google.com platform.twitter.com syndication.twitter.com" });
+            filters.Add(new CspScriptSrcAttribute { Self = true, CustomSources = "https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://platform.twitter.com https://cdn.syndication.twitter.com https://cdn.syndication.twimg.com" });
+            filters.Add(new CspStyleSrcAttribute { Self = true, UnsafeInline = true, CustomSources = "https://fonts.googleapis.com https://platform.twitter.com https://ton.twimg.com" });
+            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "https://www.giantbomb.com https://www.paypalobjects.com https://syndication.twitter.com https://pbs.twimg.com https://platform.twitter.com https://ton.twimg.com data:" });
+            filters.Add(new CspFontSrcAttribute { CustomSources = "https://fonts.gstatic.com" });
+            filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "https://www.google.com https://platform.twitter.com https://syndication.twitter.com" });
+            filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "https://www.google.com https://platform.twitter.com https://syndication.twitter.com" });
             filters.Add(new CspConnectSrcAttribute { Self = true });
             filters.Add(new CspFrameAncestorsAttribute { None = true });
             filters.Add(new CspReportUriAttribute { ReportUris = "https://mtxz.report-uri.com/r/d/csp/enforce" });
