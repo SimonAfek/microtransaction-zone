f735a8300429fa752d832507e0b313bacb6d4791
Simon AJ <simonaj92@gmail.com>
2018-04-19 17:47:28 +0200

    Whitelist abs.twimg.com in CSP to fix broken emoji in twitter timeline embed

--- DIHMT/App_Start/FilterConfig.cs
+++ DIHMT/App_Start/FilterConfig.cs
@@ -13,7 +13,7 @@ namespace DIHMT
             filters.Add(new CspDefaultSrcAttribute { None = true });
             filters.Add(new CspScriptSrcAttribute { Self = true, CustomSources = "https://www.google.com https://www.gstatic.com https://cdnjs.cloudflare.com https://platform.twitter.com https://cdn.syndication.twitter.com https://cdn.syndication.twimg.com" });
             filters.Add(new CspStyleSrcAttribute { Self = true, UnsafeInline = true, CustomSources = "https://fonts.googleapis.com https://platform.twitter.com https://ton.twimg.com" });
-            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "https://www.giantbomb.com https://www.paypalobjects.com https://syndication.twitter.com https://pbs.twimg.com https://platform.twitter.com https://ton.twimg.com data:" });
+            filters.Add(new CspImgSrcAttribute { Self = true, CustomSources = "https://www.giantbomb.com https://www.paypalobjects.com https://syndication.twitter.com https://abs.twimg.com https://pbs.twimg.com https://platform.twitter.com https://ton.twimg.com data:" });
             filters.Add(new CspFontSrcAttribute { CustomSources = "https://fonts.gstatic.com" });
             filters.Add(new CspFrameSrcAttribute { Self = true, CustomSources = "https://www.google.com https://platform.twitter.com https://syndication.twitter.com" });
             filters.Add(new CspChildSrcAttribute { Self = true, CustomSources = "https://www.google.com https://platform.twitter.com https://syndication.twitter.com" });
