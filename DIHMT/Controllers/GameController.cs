25922d07b6a2b527d537e8646aa157438854da6d
Simon AJ <simonaj92@gmail.com>
2018-06-16 19:34:46 +0200

    Store platform-ids in JS-array

--- DIHMT/Controllers/GameController.cs
+++ DIHMT/Controllers/GameController.cs
@@ -1,7 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
+﻿using System.Linq;
 using System.Web.Mvc;
 using DIHMT.Models;
 using DIHMT.Static;
--- DIHMT/Scripts/tabscripts.js
+++ DIHMT/Scripts/tabscripts.js
@@ -1,3 +1,10 @@
 ﻿$(function() {
     $("#tabs").tabs();
+    var tabTitle = "Secondary tags";
+    var tabCounter = 2;
+    var platforms = [];
+
+    $(".platform-id").each(function () {
+        platforms.push($(this).val());
+    });
 });
